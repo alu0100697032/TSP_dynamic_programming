cd17e13dbd897e6cb7cd6fc9016deff4758590ca
Víctor Hernández <alu0100697032@ull.edu.es>
2016-03-14 23:53:05 +0000

    Update Main class

--- src/global/Main.java
+++ src/global/Main.java
@@ -5,7 +5,10 @@
  */
 package global;
 
+import java.util.Scanner;
+
 import classes.BruteForce;
+import classes.DynamicProgramming;
 import classes.Greedy;
 import classes.Problem;
 import classes.Solution;
@@ -16,16 +19,60 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ReadFile readFile = new ReadFile("src/files/tsp1.txt");
+		boolean stop = true;
+		System.out.println("\tTravelling Salesman Problem (TSP)\n");
+		System.out.println("Type the path of the file with the input data:");
+		
+		Scanner userInput = new Scanner(System.in);
+		String inputFile = userInput.nextLine();
+		ReadFile readFile = new ReadFile(inputFile);
+		
 		Problem problem = new Problem(readFile.getNumberOfCities(), readFile.getDistances());
-		System.out.println(problem.toS());
+		System.out.println("\nInformation:\n" + problem.toS());
 		
 		Greedy greedy = new Greedy();
-		Solution greedySolution = greedy.solve(problem);
-		System.out.println(greedySolution.toS());
-		
 		BruteForce bruteForce = new BruteForce();
-		Solution bruteForceSolution = bruteForce.solve(problem, greedySolution);
-		System.out.println(bruteForceSolution.toS());
+		DynamicProgramming dp = new DynamicProgramming();
+		Solution solution = new Solution();
+		
+		while (stop) {
+			System.out.println("\nOpciones:\n1. Run greedy.\n2. Run exhaustive.\n3. Run dynamic programming.\n4. Run all.\n0. Stop program");
+			switch (userInput.nextInt()) {
+			case 1:
+				System.out.println("\nGreedy: ");
+				solution = greedy.solve(problem);
+				System.out.println(solution.toS());
+				break;
+			case 2:
+				System.out.println("\nExhaustive: ");
+				solution = greedy.solve(problem);
+				solution = bruteForce.solve(problem, solution);
+				System.out.println(solution.toS());
+				break;
+			case 3:
+				System.out.println("\nDynamic Programming: ");
+				solution = dp.solve(problem);
+				System.out.println(solution.toS());
+				break;
+			case 4:
+				System.out.println("\nGreedy: ");
+				solution = greedy.solve(problem);
+				System.out.println(solution.toS());
+				System.out.println("\nExhaustive: ");
+				solution = bruteForce.solve(problem, solution);
+				System.out.println(solution.toS());
+				System.out.println("\nDynamic Programming: ");
+				solution = dp.solve(problem);
+				System.out.println(solution.toS());
+				break;
+			case 5:
+				stop = false;
+				break;
+			default:
+				System.out.println("¡Error!Choose one valid option.");
+				break;
+			}
+		}
+		userInput.close();
 	}
 }
